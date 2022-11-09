6f13f66e7f0293f09d48aad5c14536910ca542a6
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2022-11-09 14:14:29 +0530

    Create Minimum_Number_of_swaps_required_to_sort_array.cpp

---
+++ Graph_Questions./Minimum_Number_of_swaps_required_to_sort_array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+
+int main(){
+
+    int n; cin>>n;
+    vector<pair<int, int>>v;
+    for (int i = 0; i < n; ++i)
+    {
+        int x; cin>>x;
+        v.push_back({x, i});
+    }
+    sort(v.begin(), v.end());
+    int moves=0;
+
+    vector<bool>vis(n, false);
+    for (int i = 0; i < n; ++i)
+    {
+        if(vis[i]== true|| v[i].second==i ){
+            continue;
+        }
+        int cy_len=0;
+        int j=i;
+        while(vis[j]==false){
+            vis[j]= true;
+            cy_len++;
+            j= v[j].second;
+        }
+        ans+=cy_len-1;
+    }
+    cout<<ans<<nl;
+
+
+}
