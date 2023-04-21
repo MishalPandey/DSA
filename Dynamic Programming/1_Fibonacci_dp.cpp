91880898131a422884a610c39e753865c35a6dfe
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2023-04-21 17:26:24 +0530

    Update 1_Fibonacci_dp.cpp

--- Dynamic Programming/1_Fibonacci_dp.cpp
+++ Dynamic Programming/1_Fibonacci_dp.cpp
@@ -1,3 +1,5 @@
+//Using Recursion
+
 #include <iostream>
 using namespace std;
 
@@ -24,4 +26,38 @@ int main(){
     cin>>n;
     cout<<FibMemoized(n)<<endl;
     return 0;
+}
+
+//Using Tabulation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+
+int main()
+{
+
+	  ios_base::sync_with_stdio(false);
+	  cin.tie(NULL); cout.tie(NULL);
+
+	  int n; cin>>n;
+
+	  int dp[n + 1];
+	  dp[0] = 0, dp[1]= 1;
+
+		  
+        for (int i = 2; i <= n; ++i)
+        {
+
+                dp[i]= dp[i-1]+ dp[i-2];
+            
+        }
+	  cout<<dp[n]<<nl;
+	  
+
+
+
+
+
 }
